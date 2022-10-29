c0224689668a0037598ed481329891cebc447b20
cbarrios <lalobarrios16@yahoo.com>
2022-10-28 21:04:04 -0400

    master: (1) clean template

---
+++ app/src/main/java/com/lalosapps/bookshelfapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.lalosapps.bookshelfapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.lalosapps.bookshelfapp.ui.theme.BookshelfAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BookshelfAppTheme {
+
+            }
+        }
+    }
+}
